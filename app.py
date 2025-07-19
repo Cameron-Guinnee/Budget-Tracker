b8e11f5d6553fe3258788706f7cb0ad1f777903c
Cameron-Guinnee <cmguinnee@gmail.com>
2025-07-19 15:23:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,7 @@ if st.button("🔄 Refresh Data"):
 conn: GSheetsConnection = st.connection("gsheets", type=GSheetsConnection)
 
 DATA_TTL_SECONDS = 10 * 60 # 10 mins
-df: pd.DataFrame = conn.read(
-    worksheet=get_worksheet(),
-    ttl=DATA_TTL_SECONDS,
-)
+df: pd.DataFrame = conn.read(worksheet=get_worksheet(), ttl=DATA_TTL_SECONDS,)
 
 # Prep dataframe
 df.columns = df.iloc[0]
