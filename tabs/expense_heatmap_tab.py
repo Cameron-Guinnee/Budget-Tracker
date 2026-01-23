3ccd764dafe44d52c6c6cf9a36ef91123b5f3b52
Cameron Guinnee <cmguinnee@gmail.com>
2026-01-22 19:31:35 -0600

    Update expense_heatmap_tab.py

--- tabs/expense_heatmap_tab.py
+++ tabs/expense_heatmap_tab.py
@@ -13,12 +13,20 @@ def expense_heatmap_tab(df: pd.DataFrame):
     m_grouped_data = m_grouped_data.loc[(m_grouped_data['Category'] != 'Income'), :]
     categories = sorted(m_grouped_data['Category'].unique())
     months = sorted(m_grouped_data['Month'].unique())
-    category_index = {c: i for i, c in enumerate(categories)}
-    data = [[0 for _ in categories]
-            for _ in months]
+    month_index = {m: i for i, m in enumerate(months)}
+    
+    #category_index = {c: i for i, c in enumerate(categories)}
+    #data = [[0 for _ in categories]
+    #        for _ in months]
+    data = [[0 for _ in categories] for _ in months]
+    
     first_month = months[0]
     for _, row in m_grouped_data.iterrows():
-        data[row['Month'] - first_month][category_index[row['Category']]] = row['Price']
+        r = month_index[int(row["Month"])]
+        c = category_index[str(row["Category"]).strip()]
+        data[r][c] = float(row["Price"])
+    #for _, row in m_grouped_data.iterrows():
+    #    data[row['Month'] - first_month][category_index[row['Category']]] = row['Price']
     heatmap = px.imshow(data, x=categories, y=months, labels=dict(color='Total Expenses'), text_auto='.2s')
     heatmap.update_yaxes(labelalias=month_labels, tickmode='linear')
     heatmap.update_traces(hovertemplate="Category: %{x}<br>Month: %{y}<br>Total: %{z:$,.2f}<extra></extra>")
