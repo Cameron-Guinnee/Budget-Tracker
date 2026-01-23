5e97bd89cc8f6ee7e3a2f7be591d59ed79ad2b49
Cameron Guinnee <cmguinnee@gmail.com>
2026-01-22 19:35:08 -0600

    Update expense_heatmap_tab.py

--- tabs/expense_heatmap_tab.py
+++ tabs/expense_heatmap_tab.py
@@ -3,6 +3,15 @@ import plotly.express as px
 import streamlit as st
 from utils import month_labels
 
+
+
+
+
+
+
+
+
+
 def expense_heatmap_tab(df: pd.DataFrame):
     df['Month'] = df['Date'].dt.month
     df = df.sort_values(by='Month')
@@ -12,15 +21,21 @@ def expense_heatmap_tab(df: pd.DataFrame):
         ], as_index=False).sum(numeric_only=True)
     m_grouped_data = m_grouped_data.loc[(m_grouped_data['Category'] != 'Income'), :]
     categories = sorted(m_grouped_data['Category'].unique())
-    months = sorted(m_grouped_data['Month'].unique())
+    months = sorted(m_grouped_data["Month"].unique())
     month_index = {m: i for i, m in enumerate(months)}
     
     category_index = {c: i for i, c in enumerate(categories)}
-    data = [[0 for _ in categories]
-        for _ in months]
+    #data = [[0 for _ in categories]
+    #    for _ in months]
+    data = [[0 for _ in categories] for _ in months]
+    
     first_month = months[0]
+    #for _, row in m_grouped_data.iterrows():
+    #    data[row['Month'] - first_month][category_index[row['Category']]] = row['Price']
     for _, row in m_grouped_data.iterrows():
-        data[row['Month'] - first_month][category_index[row['Category']]] = row['Price']
+        r = month_index[int(row["Month"])]
+        c = category_index[str(row["Category"]).strip()]
+        data[r][c] = float(row["Price"])
     heatmap = px.imshow(data, x=categories, y=months, labels=dict(color='Total Expenses'), text_auto='.2s')
     heatmap.update_yaxes(labelalias=month_labels, tickmode='linear')
     heatmap.update_traces(hovertemplate="Category: %{x}<br>Month: %{y}<br>Total: %{z:$,.2f}<extra></extra>")
