fe23284630f8205ec31d2764c32c5950cd1e5548
Cameron Guinnee <cmguinnee@gmail.com>
2026-06-12 23:08:02 -0500

    Update df_tab.py

--- tabs/df_tab.py
+++ tabs/df_tab.py
@@ -5,7 +5,9 @@ from styling import category_color_map, payment_method_color_map, payment_method
 def df_tab(df: pd.DataFrame) -> None:
     df = df.reset_index(drop=True)
     df["Price"] = df["Price"].astype(float)
-
+    st.write(df.dtypes)
+    st.write(df.index)
+    st.write(df.tail(10))
     with pd.option_context('mode.chained_assignment', None):
         df.loc[:,"Payment Method"] = df["Payment Method"].map(lambda x: f"{payment_method_label_prefix[x]} {x}")
 
@@ -19,9 +21,7 @@ def df_tab(df: pd.DataFrame) -> None:
     owner_color_map = get_owner_color_map()
     if owner_color_map:
         styled_df = styled_df.map(lambda x: f"color: {owner_color_map.get(str(x), 'black')}", subset=["Owner"]) 
-    st.write(df.dtypes)
-    st.write(df.index)
-    st.write(df.tail(10))
+   
     st.dataframe(styled_df, column_config={
         "Shared": st.column_config.CheckboxColumn(),
     }, hide_index=True, use_container_width=True)
