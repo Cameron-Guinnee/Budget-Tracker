e32975839b23e781b6d8c038d9e34f87844d2666
Cameron Guinnee <cmguinnee@gmail.com>
2026-06-12 23:47:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ DATA_TTL_SECONDS = 10 * 60 # 10 mins
 df: pd.DataFrame = conn.read(worksheet=get_worksheet(), ttl=DATA_TTL_SECONDS)
 
 # Debugging for data tab error
-with st.expander("🔍 RAW data from conn.read()"):
+
+with st.expander("RAW data from conn.read()"):
     st.write("dtypes:", df.dtypes.astype(str).to_dict())
     st.write("shape:", df.shape)
     raw_repr = df.head(5).map(lambda x: repr(x))
